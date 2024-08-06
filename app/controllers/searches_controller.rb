822f2d14877921b49cae0e1fca5b0672060f2adb
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-08-06 06:03:30 +0000

    [Add]ユーザーと投稿を選択できるフォームを追加、検索結果の一覧ページを作成

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,12 @@
 class SearchesController < ApplicationController
   def search
+    @model = params[:model]
+    @word = params[:word]
 
+    if @model == "User"
+      @users = User.all
+    else
+      @books = Book.all
+    end
   end
 end
