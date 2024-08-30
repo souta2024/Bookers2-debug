ba19d7df46da29e49ead3a486f6661a87ce43bf3
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-08-30 09:08:07 +0000

    [Finish]8a 9a

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,11 @@ class User < ApplicationRecord
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
+
+  has_many :view_counts, dependent: :destroy
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   end
   get "search"=>"searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :messages, only: [:create]
+  resources :rooms, only:[:create, :show]
 end
